88c3760a7fb8a19c870d50bf297f503e73f6cdcb
Yomy14 <133653688+Yomy14@users.noreply.github.com>
2023-06-21 12:42:36 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1: integer 1 to be added
+ * @num2: integer 2 to be added
+ * Description: give two integers, returns sum
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
