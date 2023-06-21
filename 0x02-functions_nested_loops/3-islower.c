276fea00bd64f72735a256fd3cca9e281019d064
Yomy14 <133653688+Yomy14@users.noreply.github.com>
2023-06-21 12:30:37 +0300

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Description: checks for lowercase character
+ * @c : _islower
+ * Return: 1 if true. 0 if false
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
