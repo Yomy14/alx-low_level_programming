018e55e1c05f547cb356eef57be17b4e3e9ff6e9
Yomy14 <133653688+Yomy14@users.noreply.github.com>
2023-06-23 16:41:54 +0300

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_line - return an underscore a number of n times
+ * @n: return an underscore
+ * Return: Always 0 (success)
+ */
+void print_line(int n)
+{
+	int line = 1;
+
+	for (line = 1; line <= n; line++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		else
+			_putchar('_');
+	}
+	_putchar('\n');
+}
