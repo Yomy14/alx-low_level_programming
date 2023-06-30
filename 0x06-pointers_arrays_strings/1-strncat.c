b46aed891bf26662bbab690d04cec3cccc58f706
Yomy14 <133653688+Yomy14@users.noreply.github.com>
2023-06-30 12:30:47 +0300

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strncat - Concatenates two strings
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count1 = 0, count2 = 0;
+
+	while (*(dest + count1) != '\0')
+	{
+		count1++;
+	}
+	while (count2 < n)
+	{
+		*(dest + count1) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count1++;
+		count2++;
+	}
+	return (dest);
+}
